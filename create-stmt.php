c00dcefe466f402148c65cd21f157711cf7dc472
Isaías Mendes <isaiaszmendes@gmail.com>
2018-02-23 22:57:29 -0300

    Modificado para usar como exemplo em outros proj

--- create-stmt.php
+++ create-stmt.php
@@ -3,22 +3,22 @@ include './connection.php';
 // chamada a função da conexão com o banco
 $conn = getConnection(); 
 
-$sql = 'INSERT INTO produto (descricao,qtd, valor) VALUES (:dec,:qtd,:valor)';
-// $sql = 'INSERT INTO produto (descricao,qtd, valor) VALUES (?,?,?)';
+$sql = 'INSERT INTO tbl_name (campo1,campo2, campo3) VALUES (:cmp1,:cmp2,:cmp3)';
+// $sql = 'INSERT INTO produto (campo1,campo2, campo3) VALUES (?,?,?)';
 
 $stmt = $conn->prepare($sql);
 
-$descricao = 'Açúcar';
-$qtd = 2;
-$valor =  2.80;
+$campo1 = 'Valor';
+$campo2 = 2;
+$campo3 =  2.80;
 # Passagem por parametro
-$stmt->bindParam(':dec',$descricao);
-$stmt->bindParam(':qtd',$qtd);
-$stmt->bindParam(':valor',$valor);
+$stmt->bindParam(':cmp1',$campo1);
+$stmt->bindParam(':cmp1',$campo2);
+$stmt->bindParam(':cmp1',$campo3);
 
-// $stmt->bindValue(1,'Feijão');
-// $stmt->bindValue(2,5);
-// $stmt->bindValue(3,5.50);
+// $stmt->bindValue(1,'Valor');
+// $stmt->bindValue(2,2);
+// $stmt->bindValue(3,2.80);
 
 if($stmt->execute()){
 
